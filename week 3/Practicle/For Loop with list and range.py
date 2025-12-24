a74a583020cad7ad0fba514422c9166130004154
suridhk <kcsuridh24@tbc.edu.np>
2025-12-24 20:38:56 +0545

    Delete week 3/Practicle directory
    delete

--- week 3/Practicle/For Loop with list and range.py
+++
@@ -1,9 +0,0 @@
-# List
-names = ["Alice", "Bob", "Charlie", "David"]
-for n in names:
-    print("Name:", n)
-
-# Range
-
-for n in range(2, 12, 2):  # 2,4,6,8,10
-    print(f"{n} to the power of {n} = {n**n}")
--- week 3/Practicle/MembershipTesting.py
+++
@@ -1,6 +0,0 @@
-names = ["Terry", "John", "Michael", "Eric", "Terry", "Graham"]
-print("Eric" in names)          # True
-print("Mark" not in names)      # True
-
-message = "Hello there, my name is John"
-print("nam" in message)         # True
